69744e45368fd7f1a67d30aecc408532e3e54f38
AadeshPochhi <114843062+AadeshPochhi@users.noreply.github.com>
2024-04-15 11:58:49 +0530

    Time: 72 ms (80.97%), Space: 70.2 MB (67.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         //Method 1:
-        // int ans=-1e9;
-        // int sum=0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(sum<0){
-        //         sum=0;
-        //     }
-        //     sum+=nums[i];
-        //     ans=max(ans,sum);
-        // }
-        // return ans;
+        int ans=-1e9;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(sum<0){
+                sum=0;
+            }
+            sum+=nums[i];
+            ans=max(ans,sum);
+        }
+        return ans;
         
-        // //Method 2:
+        // //Method 2:  TC:O(n)  SC:O(n)
         // int n=nums.size();
         // if(n==1) return nums[0];
         // vector<int> ans(n);
@@ -25,15 +25,17 @@ public:
         // }
         // return res;
         
-        int n=nums.size();
-        if(n==1) return nums[0];
-        int prev=nums[0];
-        int res=prev;
-        for(int i=1;i<n;i++){
-            prev=max(nums[i]+prev,nums[i]);
-            res=max(res,prev);
-        }
-        return res;
+        
+        // Method 3: TC:O(n)  SC:O(1)
+        // int n=nums.size();
+        // if(n==1) return nums[0];
+        // int prev=nums[0];
+        // int res=prev;
+        // for(int i=1;i<n;i++){
+        //     prev=max(nums[i]+prev,nums[i]);
+        //     res=max(res,prev);
+        // }
+        // return res;
         
     }
 };
