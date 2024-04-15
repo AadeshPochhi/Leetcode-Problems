512225b7b3bb13845bdba48533b257d938c6b194
AadeshPochhi <114843062+AadeshPochhi@users.noreply.github.com>
2024-04-15 11:52:01 +0530

    Time: 70 ms (87.07%), Space: 73 MB (5.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         //Method 1:
-        int ans=-1e9;
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            if(sum<0){
-                sum=0;
-            }
-            sum+=nums[i];
-            ans=max(ans,sum);
-        }
-        return ans;
+        // int ans=-1e9;
+        // int sum=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(sum<0){
+        //         sum=0;
+        //     }
+        //     sum+=nums[i];
+        //     ans=max(ans,sum);
+        // }
+        // return ans;
         
         // //Method 2:
-        // int n=nums.size();
-        // vector<int> ans(n);
-        // ans[0]=nums[0];
-        // for(int i=1;i<n;i++){
-        //     if(nums[i]>(nums[i]+ans[i-1])) ans[i]=nums[i];
-        //     else{
-        //         ans[i]=nums[i]+ans[i-1];
-        //     }
-        // }
-        // return *max_element(ans.begin(),ans.end());
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> ans(n);
+        ans[0]=nums[0];
+        int res=ans[0];
+        for(int i=1;i<n;i++){
+            ans[i]=max(nums[i]+ans[i-1],nums[i]);
+            res=max(res,ans[i]);
+        }
+        return res;
         
     }
 };
