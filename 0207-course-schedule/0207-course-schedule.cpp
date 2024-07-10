bb446aeaf211da0cd77dbe44765e7445bfaee730
AadeshPochhi <114843062+AadeshPochhi@users.noreply.github.com>
2024-07-11 02:51:23 +0530

    Time: 19 ms (36.31%), Space: 17.5 MB (39.79%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool dfs(int v, vector<int> adj[], vector<int> &vis, vector<int> &stack){
+        if(stack[v]==1) return false;
+        if(vis[v]==1) return true;
+        vis[v]=1;
+        stack[v]=1;
+        for(int it:adj[v]){
+            if(!dfs(it,adj,vis,stack)){
+                return false;
+            }
+        }
+        stack[v]=0;
+        return true;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for(auto p:prerequisites){
+            int u=p[0];
+            int v=p[1];
+            adj[u].push_back(v);
+        }
+        vector<int>vis(numCourses,0);
+        vector<int>stack(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            if(!dfs(i,adj,vis,stack)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
