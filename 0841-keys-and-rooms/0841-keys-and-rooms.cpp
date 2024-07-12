f5173bbbbf73341c2a817255b76e78d87d5f15d2
AadeshPochhi <114843062+AadeshPochhi@users.noreply.github.com>
2024-07-13 02:39:48 +0530

    Time: 8 ms (43.93%), Space: 13.9 MB (25.79%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& rooms, int src, vector<bool> &vis){
-        vis[src]=true;
+//     void dfs(vector<vector<int>>& rooms, int src, vector<bool> &vis){
+//         vis[src]=true;
         
-        for(int &node:rooms[src]){
-            if(!vis[node])  
-                dfs(rooms,node,vis);
-        } 
-    }
+//         for(int &node:rooms[src]){
+//             if(!vis[node])  
+//                 dfs(rooms,node,vis);
+//         } 
+//     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
         vector<bool>vis(n,false);
         
-        dfs(rooms,0,vis);
+//         dfs(rooms,0,vis);
         
+//         for(bool x:vis){
+//             if(!x)
+//                 return false;
+//         }
+//         return true;
+        queue<int>q;
+        q.push(0);
+        while(!q.empty()){
+            int node=q.front(); q.pop();
+            vis[node]=true;
+            for(int i=0;i<rooms[node].size();i++){
+                if(!vis[rooms[node][i]])
+                    q.push(rooms[node][i]);
+            }
+        }
+                
         for(bool x:vis){
             if(!x)
                 return false;
