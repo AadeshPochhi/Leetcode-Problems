becf74a302d834d1c6b1e2403ef291cc42f7afa4
AadeshPochhi <114843062+AadeshPochhi@users.noreply.github.com>
2024-07-16 06:38:08 +0530

    Time: 126 ms (55.34%), Space: 72.7 MB (23.95%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if(trust.size()<n-1) return -1;
+        if(n==1) return 1;
         vector<int>score(n+1,0);
         for(auto edge:trust){
             score[edge[0]]--;
             score[edge[1]]++;
         }
-        for(int i=1;i<score.size();i++){
-            if(score[i]==n-1) return i;
+        auto it=find(score.begin(),score.end(),n-1);
+        if(it!=score.end()){
+            return distance(score.begin(),it);
         }
         return -1;
     }
