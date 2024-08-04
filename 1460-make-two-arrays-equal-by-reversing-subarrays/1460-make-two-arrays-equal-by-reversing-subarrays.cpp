b98dc77a31a44b4cfc3a31a43875b6028fc13db8
AadeshPochhi <114843062+AadeshPochhi@users.noreply.github.com>
2024-08-04 10:05:44 +0530

    Time: 12 ms (34.18%), Space: 19.5 MB (25.47%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -8,30 +8,9 @@ public:
             m[arr[i]]++;
         }
         for(int i=0;i<n;i++){
-            if(m.find(target[i])==m.end()){
-                return false;
-            }
-            else{
-                m[target[i]]--;
-                if(m[target[i]]==0) m.erase(target[i]);
-            }
+            if(!m[target[i]]) return false;
+            m[target[i]]--;
         }
-        if(m.empty()){
-            return true;
-        }
-        return false;
-        
-        
-        // if (target.size() != arr.size()) return false;
-        // unordered_map<int, int> m;
-        // for (int i = 0; i < target.size(); i++) {
-        //     m[target[i]]++;
-        // }
-        // for (int i = 0; i < arr.size(); i++) {
-        //     if (!m[arr[i]]) return false;
-        //     m[arr[i]]--;
-        // }
-        // return true;
-
+        return true;
     }
 };
