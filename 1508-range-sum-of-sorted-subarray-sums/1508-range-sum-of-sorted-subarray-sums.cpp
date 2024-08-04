6636649a8e0ae0f778c61b06441118260155f77c
AadeshPochhi <114843062+AadeshPochhi@users.noreply.github.com>
2024-08-05 00:59:59 +0530

    Time: 130 ms (9.66%), Space: 27.2 MB (31.24%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int mod=1e9+7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int>subarrays;
+        priority_queue<int, vector<int>, greater<int>> subarrays;
         int prefix;
         for(int i=0;i<n;i++){
             prefix=0;
             for(int j=i;j<n;j++){
                 prefix+=nums[j];
                 prefix%=mod;
-                subarrays.push_back(prefix);
+                subarrays.push(prefix);
             }
         }
-        sort(subarrays.begin(),subarrays.end());
         int res=0;
-        for(int i=left-1;i<=(right-1);i++){
-            res+=subarrays[i];
-            res%=mod;
+        for(int i=1;i<=right;i++){
+            int val=subarrays.top();
+            subarrays.pop();
+            if(i>=left){
+                res+=val;
+                res%=mod;
+            }
         }
         return res;
     }
